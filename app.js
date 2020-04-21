8d8f29f2a6a01c19d21ece5aa2a559df59cc5504
kenwoo9y <ei.1px.9.01@gmail.com>
2020-04-21 21:32:19 +0900

    register service worker

---
+++ app.js
@@ -0,0 +1,16 @@
+// register service worker
+
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
+    .then(function(registration){
+        if (registration.installing) {
+            console.log('Service Worker installing');
+        } else if (registration.waiting) {
+            console.log('Service Worker installed');
+        } else if (registration.active) {
+            console.log('Service Worker active');
+        }
+    }).catch(function(error){
+        console.log('Registration failed with ' + error);
+    })
+}
